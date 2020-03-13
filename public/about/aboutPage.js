ecd88aa44f874e695928393a04dd1f6c0f73c31f
George Raduta <george.raduta@cern.ch>
2020-03-13 12:29:19 +0100

    Add router to buttons clicks

--- public/about/aboutPage.js
+++ public/about/aboutPage.js
@@ -1,6 +1,22 @@
-import {h} from '/js/src/index.js';
+import {h, iconHome, iconDataTransferDownload} from '/js/src/index.js';
 
 export default (model) =>
   [
-    h('', 'About page content')
+    h('', 'About page content'),
+    createButtonGroup(model)
   ];
+
+const createButtonGroup = (model) =>
+  h('.w-25', [
+    h('.btn-group', [
+      h('button', {
+        title: 'Home router',
+        class: 'btn btn-success',
+        onclick: () => model.router.go('?page=home')
+      }, ['Home', ' ', iconHome()]
+      ),
+      h('button.btn.btn-primary', {
+        onclick: () => console.log('Get me some data about the project')
+      }, iconDataTransferDownload())
+    ])
+  ])
--- public/home/homePage.js
+++ public/home/homePage.js
@@ -8,8 +8,8 @@ export default (model) =>
         h('button.btn.btn-success', {
           title: 'About router',
           style: 'color: red',
-          onclick: () => console.log('Button should take me to about page')
-        }, ['About', ' ', info()]
+          onclick: () => model.router.go('?page=about')
+        }, [h('label.ph2', 'About'), info()]
         ),
         h('button.btn.btn-primary', {
           onclick: () => console.log('get me the user')
