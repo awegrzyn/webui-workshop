a9edb5082216757846b7d8f2155948ad75df6396
George Raduta <george.raduta@cern.ch>
2020-03-13 12:15:25 +0100

    Add buttons on home page

--- public/home/homePage.js
+++ public/home/homePage.js
@@ -1,6 +1,19 @@
-import {h} from '/js/src/index.js';
+import {h, info, iconPerson} from '/js/src/index.js';
 
 export default (model) =>
   [
-    h('', 'THis is the new content for home page')
+    h('.w-100', 'This is the new content for home page'),
+    h('.w-25', [
+      h('.btn-group', [
+        h('button.btn.btn-success', {
+          title: 'About router',
+          style: 'color: red',
+          onclick: () => console.log('Button should take me to about page')
+        }, ['About', ' ', info()]
+        ),
+        h('button.btn.btn-primary', {
+          onclick: () => console.log('get me the user')
+        }, iconPerson())
+      ])
+    ])
   ];
